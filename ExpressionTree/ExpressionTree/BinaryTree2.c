71a05d6d472c46500adcef54c2d9b5a3dd469749
user <user@USER-PC>
2017-02-26 18:19:34 +0900

    ExpressionTree

---
+++ ExpressionTree/ExpressionTree/BinaryTree2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinaryTree2.h"
+
+BTreeNode * MakeBTreeNode(void)
+{
+	BTreeNode * newNode = (BTreeNode *)malloc(sizeof(BTreeNode));
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return newNode;
+}
+
+BTData GetData(BTreeNode * bt)
+{
+	return bt->data;
+}
+
+void SetData(BTreeNode * bt, BTData data)
+{
+	bt->data = data;
+}
+
+BTreeNode * GetLeftSubTree(BTreeNode * bt)
+{
+	return  bt->left;
+}
+
+BTreeNode * GetRightSubTree(BTreeNode * bt)
+{
+	return bt->right;
+}
+
+void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub)
+{
+	if (main->left != NULL)
+		free(main->left);
+
+	main->left = sub;
+}
+
+void MakeRightSubTree(BTreeNode * main, BTreeNode * sub)
+{
+	if (main->right != NULL)
+		free(main->right);
+
+	main->right = sub;
+}
+
+void PreorderTraverse(BTreeNode * bt, VisitFuncPtr action)
+{
+	if (bt == NULL)
+		return;
+
+	action(bt->data);
+	PreorderTraverse(bt->left, action);
+	PreorderTraverse(bt->right, action);
+}
+
+void InorderTraverse(BTreeNode * bt, VisitFuncPtr action)
+{
+	if (bt == NULL)
+		return;
+
+	InorderTraverse(bt->left, action);
+	action(bt->data);
+	InorderTraverse(bt->right, action);
+}
+
+void PostorderTraverse(BTreeNode * bt, VisitFuncPtr action)
+{
+	if (bt == NULL)
+		return;
+
+	PostorderTraverse(bt->left, action);
+	PostorderTraverse(bt->right, action);
+	action(bt->data);
+}
---
+++ ExpressionTree/ExpressionTree/ListBaseStack.h
@@ -0,0 +1,31 @@
+#ifndef __LB_STACK_H__
+#define __LB_STACK_H__
+
+#include "BinaryTree2.h"
+
+#define TRUE 1
+#define FALSE 0
+
+typedef BTreeNode *  Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node * next;
+}Node;
+
+typedef struct _LBstack
+{
+	Node * head;
+} ListStack;
+
+typedef ListStack Stack;
+
+void StackInit(Stack * pstack);
+int SIsEmpty(Stack * pstack);
+
+void SPush(Stack * pstack, Data data);
+Data SPop(Stack * pstack);
+Data SPeek(Stack * pstack);
+
+#endif // !__LB_STACK_H__
